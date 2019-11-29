e7031196a754e4783efe9f0a44b0f6478df89caa
Araujo <arthur.araujo@capgemini.com>
2019-11-29 17:09:25 -0300

    Tela de minhas configurações finalizada

--- src/app/configuracoes/perfil/perfil.page.ts
+++ src/app/configuracoes/perfil/perfil.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController  } from '@ionic/angular';
+import { ModalController, LoadingController  } from '@ionic/angular';
+
+import { AutenticacaoService } from '../../services/autenticacao.service'
 
 @Component({
   selector: 'app-perfil',
@@ -8,13 +10,43 @@ import { ModalController  } from '@ionic/angular';
 })
 export class PerfilPage implements OnInit {
 
-  constructor(public modalController: ModalController) { }
+  loading: any;
+  dadosUsuario: any = {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+  }
+
+  constructor(public modalController: ModalController, public AutenticacaoService: AutenticacaoService, public loadingController: LoadingController) {
+    this.iniciaDadosUsuario();
+   }
 
   ngOnInit() {
   }
 
   closeModal() {
     this.modalController.dismiss();
+  }
+
+  iniciaDadosUsuario(){
+
+    this.presentLoading("Validando acesso, aguarde...");
+
+    this.AutenticacaoService.get("https://anypoint.mulesoft.com/mocking/api/v1/links/a17efb3a-fb82-4593-9eae-381aeb108192/user-info")
+        .subscribe( result => {
+              this.dadosUsuario = result.json();
+              this.loading.onDidDismiss();             
+        });
+        
+  }
+
+  async presentLoading(message) {
+    this.loading = await this.loadingController.create({
+      message: message,
+      duration: 2000
+    });
+    await this.loading.present();    
   }
 
 }
