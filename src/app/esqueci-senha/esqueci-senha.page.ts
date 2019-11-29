757451b1b3e3c838ffdedf2af472bd52f6a91ea5
Araujo <arthur.araujo@capgemini.com>
2019-11-29 12:19:34 -0300

    Esqueci minha senha finalizado - Falta API

--- src/app/esqueci-senha/esqueci-senha.page.ts
+++ src/app/esqueci-senha/esqueci-senha.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {  IonInput, AlertController, NavController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-esqueci-senha',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EsqueciSenhaPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('email', { static:false })  inputemail: IonInput;
+
+  formEsqueci: any = {
+      email: ""
+  }
+
+loading: any;
+
+  constructor(public navCtrl: NavController, private alertController: AlertController, public loadingController: LoadingController) { }
 
   ngOnInit() {
+  }
+
+
+  esqueci_senha(){
+
+    if(this.formEsqueci.email===""){  this.inputemail.setFocus(); }
+    else{
+      
+      this.presentLoading("Efetuando solicitação de nova senha, aguarde...");
+      
+      setTimeout( () => {
+        
+          this.presentAlert("Equeci minha senha", "", "Link para gerar nova senha enviada por e-mail.").then(()=>{
+              this.navCtrl.navigateRoot('/login');
+          })
+          
+      }, 3000);
+      
+    }
+
+  }
+
+  async presentAlert(title, subTitle, mensage) {
+    const alert = await this.alertController.create({
+      header: title,
+      subHeader: subTitle,
+      message: mensage,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async presentLoading(message) {
+    this.loading = await this.loadingController.create({
+      message: message,
+      duration: 2000
+    });
+    await this.loading.present();    
   }
 
 }
